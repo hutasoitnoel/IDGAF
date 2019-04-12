dcb2491e2703268d9a92aef360cf10dc644cd820
ramdhon <ramadhonimuhammad@gmail.com>
2019-04-12 14:25:11 +0700

    solved bugs

--- server/helpers/googleUpload.js
+++ server/helpers/googleUpload.js
@@ -62,8 +62,9 @@ const uploads = (req, res, next) => {
   const base64Data = image.replace(/^data:image\/png;base64,|^data:image\/gif;base64,/, "");
   const newFilename = Date.now() + '.' + 'gif';
   const newFile = 'uploads/' + newFilename;
+  req.fileName = newFilename
   req.filePath = newFile
-  console.log(newFile, 'NAMA FILE YANG DI UPLOADS FOLDER')
+  // console.log(newFile, 'NAMA FILE YANG DI UPLOADS FOLDER')
   fs.writeFile(newFile, base64Data, 'base64', function (err) {
       if (err) {
           console.log(err);
@@ -78,15 +79,25 @@ const goToGCS = (req, res) => {
   //-
   // Upload a file from a local path.
   //-
-  bucket.upload('../' + req.file.path, function(err, file, apiResponse) {
+  bucket.upload(req.filePath, function(err, file, apiResponse, next) {
     // Your bucket now contains:
     // - "image.png" (with the contents of `/local/path/image.png')
   
     // `file` is an instance of a File object that refers to your new file.
-    console.log(err);
-    console.log(file);
-    console.log(apiResponse);
-    res.send({err, file, apiResponse});
+    // console.log(err);
+    // console.log('FILE ============')
+    // console.log(file);
+    // console.log('API ============')
+    // console.log(apiResponse);
+    req.fileUrl = getPublicUrl(req.fileName);
+    // Assuming that 'path/file.txt' is a regular file.
+    fs.unlink(req.filePath, (err) => {
+      if (err) throw err;
+      console.log(`${req.filePath} was deleted`);
+    });
+
+    next();
+    // res.send({err, file, apiResponse});
   });
   
 }
--- server/routes/post.js
+++ server/routes/post.js
@@ -2,26 +2,26 @@ const router = require("express").Router();
 const postController = require("../controllers/postController");
 const tagController = require('../controllers/tagController')
 const upload = require('../helpers/googleUpload');
-const tagController = require('../controllers/tagController');
+// const tagController = require('../controllers/tagController');
 
 //get all posts data
-// router.get("/", postController.findAll);
+router.get("/", postController.findAll);
 
 //get one post data find by any
-// router.get("/:id", postController.find);
+router.get("/:id", postController.find);
 
 //update a post
-// router.patch("/:id", postController.update)
+router.patch("/:id", postController.update)
 
 //delete a post
-// router.delete("/:id", postController.delete)
+router.delete("/:id", postController.delete)
 
 //register new post
-router.post("/", postController.create);
+router.post("/", upload.uploads, upload.goToGCS, postController.create);
 
 //generate tags
 // router.post("/tags", upload.multer.single('image'), upload.sendUploadToGCS, tagController.create, postController.generateTags);
-router.post("/tags", upload.uploads, upload.goToGCS);
+// router.post("/tags", upload.uploads, upload.goToGCS);
 
 
 module.exports = router;
