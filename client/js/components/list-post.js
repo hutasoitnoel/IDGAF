187d5d13301d69b0fe7d575b4d7dbe72f5ed820a
eltim12 <eltim2000@gmail.com>
2019-04-12 12:31:51 +0700

    constructing client side

---
+++ client/js/components/list-post.js
@@ -0,0 +1,51 @@
+Vue.component("list-post",{
+    props: [],
+    template: `
+    <div class="container">
+    <div class="grid">
+        <figure class="effect-milo">
+            <img src="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                alt="img11" />
+            <figcaption>
+                <div class="fb-share-button fb-config"
+                    data-href="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                    data-layout="button_count">
+                </div>
+
+                <h2><span>anjing</span></h2>
+                    <p class="high">#happy #sad #funny #dog</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-milo">
+            <img src="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                alt="img11" />
+            <figcaption>
+                <div class="fb-share-button fb-config"
+                    data-href="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                    data-layout="button_count">
+                </div>
+
+                <h2><span>anjing</span></h2>
+                    <p class="high">#happy #sad #funny #dog</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-milo">
+            <img src="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                alt="img11" />
+            <figcaption>
+                <div class="fb-share-button fb-config"
+                    data-href="https://media3.giphy.com/media/SggILpMXO7Xt6/giphy.gif?cid=790b76115caff44258592e5832098a3f"
+                    data-layout="button_count">
+                </div>
+
+                <h2><span>anjing</span></h2>
+                    <p class="high">#happy #sad #funny #dog</p>
+                <a href="#">View more</a>
+            </figcaption>
+        </figure>
+    </div>
+</div>
+    `
+})
